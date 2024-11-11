0a66f702802ebc0107cfe2a4f0be9eb49b253fc2
KAYODE ADEDIWURA <kayodeadediwura@KAYODEs-MacBook-Air.local>
2024-11-11 23:21:13 +0000

    testing terraform init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Using Git Action to create a pipeline that automates deployment of an EC2 machine- CICD
+Store the state file codes in a Backend S3 configuration
--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-#Create Security Group for Frontend Node 1  EC2 Instances
+resource "aws_security_group" "project" {
 
-resource "aws_security_group" {
-  name = "allow_ssh"
-  # ... other configuration ...
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -17,6 +15,26 @@ resource "aws_security_group" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+resource "aws_instance" "node_1" {
+  ami                    = var.node_1_ami
+  instance_type          = var.node_1_instance_type
+  key_name               = var.node_1_key_name
+  subnet_id              = var.node_1_subnet_id
+  vpc_security_group_ids = [aws_security_group.project.id]
+
+  tags = {
+    Name = "node_1"
   }
 }
 
