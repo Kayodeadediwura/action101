e79f9ce12f3b380328d4baf1d92d17c3d9df996c
KAYODE ADEDIWURA <kayodeadediwura@KAYODEs-MacBook-Air.local>
2024-11-12 18:04:14 +0000

    run terraform apply

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+resource "aws_instance" "node_1" {
+  ami                    = "ami-02f617729751b375a"
+  instance_type          = "tc2 micro"
+  key_name               = "kokoro101"
+  vpc_security_group_ids = [aws_security_group.project.id]
+
+  tags = {
+    Name = "node_1"
+  }
+}
+
 resource "aws_security_group" "project" {
-
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -15,25 +24,6 @@ resource "aws_security_group" "project" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-
-resource "aws_instance" "node_1" {
-  ami                    = "ami-02f617729751b375a"
-  instance_type          = "tc2 micro"
-  key_name               = "kokoro101"
-  vpc_security_group_ids = [aws_security_group.project.id]
-
-  tags = {
-    Name = "node_1"
   }
 }
 
