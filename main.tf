945ef2b2056879ed7979e632214859050a6fd4c8
KAYODE ADEDIWURA <kayodeadediwura@KAYODEs-MacBook-Air.local>
2024-11-12 17:44:26 +0000

    activated access/secret keys

--- main.tf
+++ main.tf
@@ -27,10 +27,9 @@ resource "aws_security_group" "project" {
 
 
 resource "aws_instance" "node_1" {
-  ami                    = var.node_1_ami
-  instance_type          = var.node_1_instance_type
-  key_name               = var.node_1_key_name
-  subnet_id              = var.node_1_subnet_id
+  ami                    = "ami-02f617729751b375a"
+  instance_type          = "tc2 micro"
+  key_name               = "kokoro101"
   vpc_security_group_ids = [aws_security_group.project.id]
 
   tags = {
