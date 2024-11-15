aabb41216986fb6f3c1394ce018db23ae33c7209
KAYODE ADEDIWURA <kayodeadediwura@KAYODEs-MacBook-Air.local>
2024-11-15 21:45:37 +0000

    correcting the vpc-id

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "node_1" {
   ami           = "ami-02f617729751b375a"
   instance_type = "tc2.micro"
   key_name      = "kokoro101"
-  vpc_id           = "vpc-00de41b8a9cb1e85d"
 
 
 
@@ -12,15 +11,15 @@ resource "aws_instance" "node_1" {
 }
 
 resource "aws_security_group" "project" {
-  name = "project"
-  # ... other configuration ...
+  name   = "project"
+  vpc_id = "vpc-00de41b8a9cb1e85d"
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    
+
   }
 
   ingress {
