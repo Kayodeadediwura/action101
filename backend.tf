53cb1e8eb3f2bc6fec12ea9eaf8f9882febbd087
KAYODE ADEDIWURA <kayodeadediwura@KAYODEs-MacBook-Air.local>
2024-11-17 21:11:05 +0000

    correcting s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "primoris-terraform-store1"
+    bucket = "primoris-terraform-store2"
     key    = "env/action/terraform.tfstate"
     region = "eu-west-1"
   }
